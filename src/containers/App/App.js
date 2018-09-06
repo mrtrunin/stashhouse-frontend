7aa7e2f385a953403607bdee70396f42c39f6af5
mrtrunin <lars.trunin@gmail.com>
2018-09-06 08:47:52 +0300

    Fixing refresh token v2

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Router from "containers/App/Router";
 import NavBar from "containers/App/NavBar/NavBar";
 import { AppTheme } from "./AppTheme";
@@ -6,146 +6,21 @@ import { AppTheme } from "./AppTheme";
 import MuiPickersUtilsProvider from "material-ui-pickers/utils/MuiPickersUtilsProvider";
 import MomentUtils from "material-ui-pickers/utils/moment-utils";
 
-import { CssBaseline, LinearProgress } from "@material-ui/core";
+import { CssBaseline } from "@material-ui/core";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 
-import Message from "components/Message/Message";
-
-import axios from "axios";
-import store from "store";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import * as loginActions from "containers/Login/LoginActions";
-
-// import refreshToken from "containers/Login/LoginActions";
-
-class App extends Component {
-  componentDidMount = () => {
-    this.interceptApiResponse();
-  };
-
-  interceptApiResponse = () => {
-    axios.interceptors.request.use(
-      config => {
-        const token = localStorage.getItem("jwtToken");
-        const url = process.env.REACT_APP_SERVER_URL;
-        const tokenAuthUrl = url + "/auth/token";
-
-        if (token && !config.url.startsWith(tokenAuthUrl)) {
-          config.headers["Authorization"] = "Bearer " + token;
-        }
-        if (!token && !config.url.startsWith(tokenAuthUrl)) {
-          // Don't execute request if no authorization token
-          return;
-        }
-        return config;
-      },
-      error => {
-        return Promise.reject(error);
-      }
-    );
-
-    axios.interceptors.response.use(
-      response => {
-        return response;
-      },
-      error => {
-        const url = process.env.REACT_APP_SERVER_URL;
-        const tokenAuthUrl = url + "/auth/token/";
-
-        const unauthorized = error.response.status === 401;
-
-        const hasAuthTokenExpired =
-          localStorage.jwtToken &&
-          error.config &&
-          error.response &&
-          error.response.status === 401;
-
-        const hasRefreshTokenExpired =
-          error.config.url === tokenAuthUrl &&
-          error.response &&
-          error.response.status === 401;
-
-        const isBadRequest = error.response.status === 400;
-
-        if (isBadRequest) {
-          Message("Bad request!", "error", 400);
-          return Promise.reject(error);
-        }
-
-        if (hasAuthTokenExpired) {
-          return new Promise(async () => {
-            await this.checkTokenExpiration();
-            error.config.headers.Authorization =
-              (await "Bearer ") + localStorage.jwtToken;
-            await axios.request(error.config);
-            return (document.location = window.location.href);
-          });
-        }
-
-        if (hasRefreshTokenExpired) {
-          this.logout();
-          return Promise.reject(error);
-        }
-
-        if (unauthorized) {
-          Message("Unauthorized request", "error", 401);
-          this.logout();
-          return Promise.reject(error);
-        }
-
-        return Promise.reject(error);
-      }
-    );
-  };
-
-  checkTokenExpiration = async () => {
-    let expiration_time = localStorage.jwtToken_expiration_time;
-    let now = Date.now();
-    const {
-      actions: { refreshToken }
-    } = this.props;
-
-    if (localStorage.refresh_token && expiration_time - now < 82800 * 1000) {
-      try {
-        await refreshToken();
-      } catch (error) {
-        Message("Could not refresh token: " + error, "error");
-        this.logout();
-      }
-    }
-  };
-
-  logout = () => {
-    return new Promise(async () => {
-      await localStorage.clear();
-      await store.dispatch({ type: "USER_LOGOUT" });
-    });
-  };
-
-  render() {
-    return (
-      <React.Fragment>
-        <MuiThemeProvider theme={AppTheme}>
-          <CssBaseline />
-          <NavBar />
-          <MuiPickersUtilsProvider utils={MomentUtils}>
-            <Router />
-          </MuiPickersUtilsProvider>
-        </MuiThemeProvider>
-      </React.Fragment>
-    );
-  }
-}
-
-const mapStateToProps = state => {
-  return {};
+const App = () => {
+  return (
+    <React.Fragment>
+      <MuiThemeProvider theme={AppTheme}>
+        <CssBaseline />
+        <NavBar />
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+          <Router />
+        </MuiPickersUtilsProvider>
+      </MuiThemeProvider>
+    </React.Fragment>
+  );
 };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    actions: bindActionCreators({ ...loginActions }, dispatch)
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/containers/App/Main/Main.js
+++ src/containers/App/Main/Main.js
@@ -1,40 +1,156 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 import { withStyles, Grid } from "@material-ui/core";
 import MessageBox from "components/Message/MessageBox";
 import MainStyle from "./MainStyle";
 
-const Main = props => {
-  const { classes } = props;
+import Message from "components/Message/Message";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 
-  const isLandingPage = window.location.pathname === "/";
+import axios from "axios";
+import store from "store";
 
-  if (isLandingPage) {
-    return (
-      <Grid
-        container
-        className={[classes.root, classes.font, classes.landingPage].join(" ")}
-        spacing={16}
-      >
-        <Grid item xs={12}>
-          {props.children}
+import * as loginActions from "containers/Login/LoginActions";
+
+class Main extends Component {
+  componentDidMount = () => {
+    this.interceptApiResponse();
+  };
+
+  interceptApiResponse = () => {
+    axios.interceptors.request.use(
+      config => {
+        const token = localStorage.getItem("jwtToken");
+        const url = process.env.REACT_APP_SERVER_URL;
+        const tokenAuthUrl = url + "/auth/token";
+
+        if (token && !config.url.startsWith(tokenAuthUrl)) {
+          config.headers["Authorization"] = "Bearer " + token;
+        }
+        if (!token && !config.url.startsWith(tokenAuthUrl)) {
+          // Don't execute request if no authorization token
+          return;
+        }
+        return config;
+      },
+      error => {
+        return Promise.reject(error);
+      }
+    );
+
+    axios.interceptors.response.use(
+      response => {
+        return response;
+      },
+      error => {
+        const url = process.env.REACT_APP_SERVER_URL;
+        const tokenAuthUrl = url + "/auth/token/";
+
+        const unauthorized = error.response.status === 401;
+
+        const hasAuthTokenExpired =
+          localStorage.jwtToken &&
+          error.config &&
+          error.response &&
+          error.response.status === 401;
+
+        const hasRefreshTokenExpired =
+          error.config.url === tokenAuthUrl &&
+          error.response &&
+          error.response.status === 401;
+
+        const isBadRequest = error.response.status === 400;
+
+        if (isBadRequest) {
+          Message("Bad request!", "error", 400);
+          return Promise.reject(error);
+        }
+
+        if (hasAuthTokenExpired) {
+          return new Promise(async () => {
+            await this.checkTokenExpiration();
+            error.config.headers.Authorization =
+              (await "Bearer ") + localStorage.jwtToken;
+            await axios.request(error.config);
+            return (document.location = window.location.href);
+          });
+        }
+
+        if (hasRefreshTokenExpired) {
+          this.logout();
+          return Promise.reject(error);
+        }
+
+        if (unauthorized) {
+          Message("Unauthorized request", "error", 401);
+          this.logout();
+          return Promise.reject(error);
+        }
+
+        return Promise.reject(error);
+      }
+    );
+  };
+
+  checkTokenExpiration = async () => {
+    let expiration_time = localStorage.jwtToken_expiration_time;
+    let now = Date.now();
+    const {
+      actions: { refreshToken }
+    } = this.props;
+
+    if (localStorage.refresh_token && expiration_time - now < 82800 * 1000) {
+      try {
+        await refreshToken();
+      } catch (error) {
+        Message("Could not refresh token: " + error, "error");
+        this.logout();
+      }
+    }
+  };
+
+  logout = () => {
+    return new Promise(async () => {
+      await localStorage.clear();
+      await store.dispatch({ type: "USER_LOGOUT" });
+    });
+  };
+
+  render() {
+    const { classes, children } = this.props;
+
+    const isLandingPage = window.location.pathname === "/";
+
+    if (isLandingPage) {
+      return (
+        <Grid
+          container
+          className={[classes.root, classes.font, classes.landingPage].join(
+            " "
+          )}
+          spacing={16}
+        >
+          <Grid item xs={12}>
+            {children}
+          </Grid>
         </Grid>
+      );
+    }
+
+    return (
+      <Grid container className={[classes.root, classes.font].join(" ")}>
+        <Grid item md={1} />
+        <Grid item xs={12} md={10} className={classes.content}>
+          {children}
+        </Grid>
+        <Grid item md={1} />
+        <MessageBox />
       </Grid>
     );
   }
-
-  return (
-    <Grid container className={[classes.root, classes.font].join(" ")}>
-      <Grid item md={1} />
-      <Grid item xs={12} md={10} className={classes.content}>
-        {props.children}
-      </Grid>
-      <Grid item md={1} />
-      <MessageBox />
-    </Grid>
-  );
-};
+}
 
 Main.propTypes = {
   title: PropTypes.string,
@@ -45,4 +161,16 @@ Main.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(MainStyle)(Main);
+const mapStateToProps = state => {
+  return {};
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    actions: bindActionCreators({ ...loginActions }, dispatch)
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+  withStyles(MainStyle)(Main)
+);
