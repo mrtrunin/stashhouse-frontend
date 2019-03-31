423da6244b6c16a840a45359f760065fbb7ee105
mrtrunin <lars.trunin@gmail.com>
2019-03-31 15:56:34 +0300

    Refactoring Business to hooks

--- src/pages/Business/Business.js
+++ src/pages/Business/Business.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { TextField } from "@material-ui/core";
 import { connect } from "react-redux";
@@ -12,207 +12,198 @@ import * as businessActions from "./BusinessActions";
 import * as businessesActions from "pages/Businesses/BusinessesActions";
 import { bindActionCreators } from "redux";
 
-export class Business extends Component {
-  handleBusinessChange = e => {
-    const { name, value } = e.target;
-    const {
-      actions: { updateBusinessField }
-    } = this.props;
-
-    updateBusinessField(value, name);
-  };
-
-  handleCreateOrUpdateBusiness = async () => {
-    const {
-      business,
-      actions: { fetchBusinesses, createBusiness, updateBusiness }
-    } = this.props;
-
-    if (business.id !== undefined) {
-      await updateBusiness(
-        business.id,
-        business.name,
-        business.address,
-        business.zip_code,
-        business.city,
-        business.country,
-        business.phone_number,
-        business.registry_number,
-        business.vat_number,
-        business.email,
-        business.website,
-        business.primary_bank,
-        business.primary_account_number,
-        business.default_email_subject,
-        business.default_email_body
-      );
-    } else {
-      await createBusiness(
-        business.name,
-        business.address,
-        business.zip_code,
-        business.city,
-        business.country,
-        business.phone_number,
-        business.registry_number,
-        business.vat_number,
-        business.email,
-        business.website,
-        business.primary_bank,
-        business.primary_account_number,
-        business.default_email_subject,
-        business.default_email_body
-      );
+const Business = props => {
+  const {
+    business,
+    actions: {
+      updateBusinessField,
+      fetchBusinesses,
+      createBusiness,
+      updateBusiness
     }
+  } = props;
 
-    await fetchBusinesses();
+  const handleUpdateBusinessField = e => {
+    e.preventDefault();
+    updateBusinessField(e.target.value, e.target.name);
   };
 
-  render() {
-    const { business } = this.props;
+  useEffect(() => {
+    fetchBusinesses();
+  }, [business]);
 
-    return (
-      <Editor>
-        <EditorHeader
-          editedObject={business}
-          label="Settings"
-          editedObjectSubheader="A place where to change your business attributes"
+  return (
+    <Editor>
+      <EditorHeader
+        editedObject={business}
+        label="Settings"
+        editedObjectSubheader="A place where to change your business attributes"
+      />
+      <EditorContent>
+        <TextField
+          name="name"
+          value={business.name ? business.name : ""}
+          label="Business Name"
+          margin="dense"
+          onChange={handleUpdateBusinessField}
+          required
         />
-        <EditorContent>
-          <TextField
-            name="name"
-            value={business.name ? business.name : ""}
-            label="Business Name"
-            margin="dense"
-            onChange={this.handleBusinessChange}
-            required
-          />
 
-          <TextField
-            name="address"
-            value={business.address ? business.address : ""}
-            label="Address"
-            margin="dense"
-            onChange={this.handleBusinessChange}
-          />
-
-          <TextField
-            name="zip_code"
-            value={business.zip_code ? business.zip_code : ""}
-            label="Zip Code"
-            margin="dense"
-            onChange={this.handleBusinessChange}
-          />
-
-          <TextField
-            name="city"
-            value={business.city ? business.city : ""}
-            label="City"
-            margin="dense"
-            onChange={this.handleBusinessChange}
-          />
-
-          <TextField
-            name="country"
-            value={business.country ? business.country : ""}
-            label="Country"
-            margin="dense"
-            onChange={this.handleBusinessChange}
-          />
-
-          <TextField
-            name="phone_number"
-            value={business.phone_number ? business.phone_number : ""}
-            label="Phone Number"
-            margin="dense"
-            onChange={this.handleBusinessChange}
-          />
-
-          <TextField
-            name="email"
-            value={business.email ? business.email : ""}
-            label="Email"
-            margin="dense"
-            onChange={this.handleBusinessChange}
-          />
-
-          <TextField
-            name="website"
-            value={business.website ? business.website : ""}
-            label="Website"
-            margin="dense"
-            onChange={this.handleBusinessChange}
-          />
-
-          <TextField
-            name="registry_number"
-            value={business.registry_number ? business.registry_number : ""}
-            label="Business Registry Number"
-            margin="dense"
-            onChange={this.handleBusinessChange}
-          />
-
-          <TextField
-            name="vat_number"
-            value={business.vat_number ? business.vat_number : ""}
-            label="VAT Number"
-            margin="dense"
-            onChange={this.handleBusinessChange}
-          />
-
-          <TextField
-            name="primary_bank"
-            value={business.primary_bank ? business.primary_bank : ""}
-            label="Bank"
-            margin="dense"
-            onChange={this.handleBusinessChange}
-          />
-
-          <TextField
-            name="primary_account_number"
-            value={
-              business.primary_account_number
-                ? business.primary_account_number
-                : ""
-            }
-            label="Bank Account Number"
-            margin="dense"
-            onChange={this.handleBusinessChange}
-          />
-
-          <TextField
-            name="default_email_subject"
-            value={
-              business.default_email_subject
-                ? business.default_email_subject
-                : ""
-            }
-            label="Default Email Subject (Note: the transaction number will be added to the subject line)"
-            margin="dense"
-            onChange={this.handleBusinessChange}
-          />
-
-          <TextField
-            name="default_email_body"
-            value={
-              business.default_email_body ? business.default_email_body : ""
-            }
-            label="Default Email Body"
-            margin="dense"
-            onChange={this.handleBusinessChange}
-            multiline
-          />
-        </EditorContent>
-        <EditorButtons
-          editedObjectLabel="Business"
-          editedObject={business}
-          createAction={this.handleCreateOrUpdateBusiness}
-          updateAction={this.handleCreateOrUpdateBusiness}
+        <TextField
+          name="address"
+          value={business.address ? business.address : ""}
+          label="Address"
+          margin="dense"
+          onChange={handleUpdateBusinessField}
         />
-      </Editor>
-    );
-  }
-}
+
+        <TextField
+          name="zip_code"
+          value={business.zip_code ? business.zip_code : ""}
+          label="Zip Code"
+          margin="dense"
+          onChange={handleUpdateBusinessField}
+        />
+
+        <TextField
+          name="city"
+          value={business.city ? business.city : ""}
+          label="City"
+          margin="dense"
+          onChange={handleUpdateBusinessField}
+        />
+
+        <TextField
+          name="country"
+          value={business.country ? business.country : ""}
+          label="Country"
+          margin="dense"
+          onChange={handleUpdateBusinessField}
+        />
+
+        <TextField
+          name="phone_number"
+          value={business.phone_number ? business.phone_number : ""}
+          label="Phone Number"
+          margin="dense"
+          onChange={handleUpdateBusinessField}
+        />
+
+        <TextField
+          name="email"
+          value={business.email ? business.email : ""}
+          label="Email"
+          margin="dense"
+          onChange={handleUpdateBusinessField}
+        />
+
+        <TextField
+          name="website"
+          value={business.website ? business.website : ""}
+          label="Website"
+          margin="dense"
+          onChange={handleUpdateBusinessField}
+        />
+
+        <TextField
+          name="registry_number"
+          value={business.registry_number ? business.registry_number : ""}
+          label="Business Registry Number"
+          margin="dense"
+          onChange={handleUpdateBusinessField}
+        />
+
+        <TextField
+          name="vat_number"
+          value={business.vat_number ? business.vat_number : ""}
+          label="VAT Number"
+          margin="dense"
+          onChange={handleUpdateBusinessField}
+        />
+
+        <TextField
+          name="primary_bank"
+          value={business.primary_bank ? business.primary_bank : ""}
+          label="Bank"
+          margin="dense"
+          onChange={handleUpdateBusinessField}
+        />
+
+        <TextField
+          name="primary_account_number"
+          value={
+            business.primary_account_number
+              ? business.primary_account_number
+              : ""
+          }
+          label="Bank Account Number"
+          margin="dense"
+          onChange={handleUpdateBusinessField}
+        />
+
+        <TextField
+          name="default_email_subject"
+          value={
+            business.default_email_subject ? business.default_email_subject : ""
+          }
+          label="Default Email Subject (Note: the transaction number will be added to the subject line)"
+          margin="dense"
+          onChange={handleUpdateBusinessField}
+        />
+
+        <TextField
+          name="default_email_body"
+          value={business.default_email_body ? business.default_email_body : ""}
+          label="Default Email Body"
+          margin="dense"
+          onChange={handleUpdateBusinessField}
+          multiline
+        />
+      </EditorContent>
+      <EditorButtons
+        editedObjectLabel="Business"
+        editedObject={business}
+        createAction={() =>
+          createBusiness(
+            business.name,
+            business.address,
+            business.zip_code,
+            business.city,
+            business.country,
+            business.phone_number,
+            business.registry_number,
+            business.vat_number,
+            business.email,
+            business.website,
+            business.primary_bank,
+            business.primary_account_number,
+            business.default_email_subject,
+            business.default_email_body
+          )
+        }
+        updateAction={() =>
+          updateBusiness(
+            business.id,
+            business.name,
+            business.address,
+            business.zip_code,
+            business.city,
+            business.country,
+            business.phone_number,
+            business.registry_number,
+            business.vat_number,
+            business.email,
+            business.website,
+            business.primary_bank,
+            business.primary_account_number,
+            business.default_email_subject,
+            business.default_email_body
+          )
+        }
+      />
+    </Editor>
+  );
+};
 
 Business.propTypes = {
   actions: PropTypes.object,
--- src/pages/Businesses/BusinessesActions.js
+++ src/pages/Businesses/BusinessesActions.js
@@ -7,11 +7,10 @@ export const FETCH_BUSINESSES_REJECTED = "FETCH_BUSINESSES_REJECTED";
 
 export function fetchBusinesses() {
   return async dispatch => {
-    dispatch({ type: FETCH_BUSINESSES });
+    await dispatch({ type: FETCH_BUSINESSES });
     try {
       const { data } = await axios.get(
-        process.env.REACT_APP_SERVER_URL + "/businesses/",
-        {}
+        process.env.REACT_APP_SERVER_URL + "/businesses/"
       );
       await dispatch({
         type: FETCH_BUSINESSES_FULFILLED,
